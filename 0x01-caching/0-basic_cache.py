f653335c39b01836409de3031d6b3c387e80e34d
Kelvin <koolkt10@gmail.com>
2024-07-27 00:55:16 +0000

    docstring

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -11,11 +11,15 @@ class BasicCache(BaseCache):
     Basic Cache Class
     """
     def put(self, key, item):
+        """Adds an item in the cache.
+        """
         if key is None or item is None:
             pass
         self.cache_data[key] = item
 
     def get(self, key):
+        """Retrieves an item by key.
+        """
         if key is None or key not in self.cache_data.keys():
             return None
         return self.cache_data[key]
